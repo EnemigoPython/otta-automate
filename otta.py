200d59e5ab5e98f9d9d44a6ef3f9716233ff3ee3
Basil Eagle <basileagle@gmail.com>
2022-10-29 17:29:01 +0100

    logging and error handling

--- otta.py
+++ otta.py
@@ -1,41 +1,68 @@
-# selenium 4
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.firefox import GeckoDriverManager
 from dotenv import load_dotenv
+import traceback
 import time
 import pathlib
+import logging
 import os
 
+FILENAME = os.path.basename(__file__)
+
+def get_logger():
+    logger = logging.getLogger(FILENAME)
+    logger.setLevel(logging.INFO)
+    handler = logging.FileHandler("otta-automate.log")
+    formatter = logging.Formatter("[%(asctime)s] %(levelname)s: %(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
+
 class DriverManager:
-    def __init__(self):
-        # Start-Process firefox -ArgumentList "-P", "otta-bot"
-        options = FirefoxOptions()
-        if (profile := os.getenv("PROFILE_FILE")) and (user := os.getenv("USER")):
-            print(profile) 
-            pathroot = pathlib.WindowsPath(r"C:\\")
-            path = pathroot / "Users" / user / "AppData" / "Local" / "Mozilla" / "Firefox" / "Profiles" / profile
-            options.add_argument("--profile")
-            options.add_argument(str(path))
-        self.driver = webdriver.Firefox(
-            service=FirefoxService(GeckoDriverManager().install()), 
-            options=options
-        )
+    def __init__(self, logger: logging.Logger):
+        self.logger = logger
+        try:
+            options = FirefoxOptions()
+            if (profile := os.getenv("PROFILE_FILE")) and (user := os.getenv("USER")):
+                self.logger.info(f"Using profile found for {user}: {profile}")
+                pathroot = pathlib.WindowsPath(r"C:\\")
+                path = pathroot / "Users" / user / "AppData" / "Local" / "Mozilla" / "Firefox" / "Profiles" / profile
+                options.add_argument("--profile")
+                options.add_argument(str(path))
+            self.driver = webdriver.Firefox(
+                service=FirefoxService(GeckoDriverManager().install()), 
+                options=options
+            )
+            self.logger.info("Driver initialised")
+        except Exception as e:
+            self.logger.error(f"Crashed on startup with a {repr(e.__class__.__name__)}: view traceback below")
+            self.logger.error(traceback.format_exc())
+            raise e
     
     def __enter__(self):
         return self.driver
 
-    def __exit__(self, *args):
+    def __exit__(self, *exc):
+        if any(exc):
+            # we get the name of an exception with e.__class__.__name__, but exc[0] is not a true Exception
+            self.logger.error(f"Script crashed with a {repr(exc[0].__name__)}: view traceback below")
+            self.logger.error(traceback.format_exc())
+        else:
+            self.logger.info("Script ran to completion: exiting")
         self.driver.quit()
 
 def main():
+    logger = get_logger()
+    logger.info(f"Started execution of {FILENAME}")
     load_dotenv()
 
-    with DriverManager() as driver:
-        print("Started")
+    with DriverManager(logger) as driver:
         driver.get("https://app.otta.com/")
-        time.sleep(300)
+        # WebDriverWait(driver, 30).until()
+
 
 if __name__ == '__main__':
     main()

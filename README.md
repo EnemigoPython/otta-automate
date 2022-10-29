641877e4f0c325556c72392890a3bb3c11ddce00
Basil Eagle <basileagle@gmail.com>
2022-10-29 19:12:52 +0100

    credential exception, start building main process

---
+++ README.md
@@ -0,0 +1,5 @@
+# OTTA_AUTOMATE
+
+## GUIDE TO SETTING UP A FIREFOX PROFILE
+1. Create a new Firefox profile
+2. Copy the path to the local profile in the menu
--- otta.py
+++ otta.py
@@ -1,14 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
 from webdriver_manager.firefox import GeckoDriverManager
 from dotenv import load_dotenv
 import traceback
-import time
 import pathlib
 import logging
+import sys
 import os
+import time
 
 FILENAME = os.path.basename(__file__)
 
@@ -21,7 +24,13 @@ def get_logger():
     logger.addHandler(handler)
     return logger
 
+class NoCredentialsException(Exception):
+    """A custom error to reject execution if the proper env variables aren't set"""
+    def __init__(self):
+        super().__init__("You need 'PROFILE_FILE' and 'USER' environment variables set to run this script")
+
 class DriverManager:
+    """A wrapper to run the Selenium driver as a context manager and inject our logger"""
     def __init__(self, logger: logging.Logger):
         self.logger = logger
         try:
@@ -32,10 +41,13 @@ class DriverManager:
                 path = pathroot / "Users" / user / "AppData" / "Local" / "Mozilla" / "Firefox" / "Profiles" / profile
                 options.add_argument("--profile")
                 options.add_argument(str(path))
+            else:
+                raise NoCredentialsException()
             self.driver = webdriver.Firefox(
                 service=FirefoxService(GeckoDriverManager().install()), 
                 options=options
             )
+            self.driver.implicitly_wait(30)
             self.logger.info("Driver initialised")
         except Exception as e:
             self.logger.error(f"Crashed on startup with a {repr(e.__class__.__name__)}: view traceback below")
@@ -47,21 +59,30 @@ class DriverManager:
 
     def __exit__(self, *exc):
         if any(exc):
-            # we get the name of an exception with e.__class__.__name__, but exc[0] is not a true Exception
+            # we get the name of an exception with e.__class__.__name__, but exc[0] is not of type Exception
             self.logger.error(f"Script crashed with a {repr(exc[0].__name__)}: view traceback below")
             self.logger.error(traceback.format_exc())
         else:
             self.logger.info("Script ran to completion: exiting")
         self.driver.quit()
 
+class JobApplication:
+    """In this class we will gather the data and formulate our application"""
+    def __init__(self):
+        pass
+
 def main():
     logger = get_logger()
     logger.info(f"Started execution of {FILENAME}")
+    if "--auto" in sys.argv:
+        logger.info("This execution was run automatically")
     load_dotenv()
 
     with DriverManager(logger) as driver:
-        driver.get("https://app.otta.com/")
-        # WebDriverWait(driver, 30).until()
+        driver.get("https://app.otta.com/jobs/theme/apply-via-otta")
+        end_of_card = driver.find_element(By.XPATH, "//*[data-testid='share-job-link']")
+        print(end_of_card.text)
+        print("YaTa")
 
 
 if __name__ == '__main__':
